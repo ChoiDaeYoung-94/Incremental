8f47e80d7b2f02b2497e3bab8fee7cbfd3816b12
ChoiDaeYoung <chleodud1410@gmail.com>
2022-02-18 00:00:57 +0900

    [Update] 대미지 간단히 연출 => 골드나 경험치 아이템 등을 얻을 시 왼쪽 사이드에 연출 띄어주는 부분 작업 해보자

--- Incremental/Assets/04. Objects/ETC/TMP_Damage.cs
+++ Incremental/Assets/04. Objects/ETC/TMP_Damage.cs
@@ -6,19 +6,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
 using DG.Tweening;
 
 public class TMP_Damage : MonoBehaviour
 {
+    [Header("--- 미리 가지고 있어야 할 data ---")]
+    [SerializeField, Tooltip("RTR - 미리 받아 두기 위함")]
+    RectTransform _rtr_this = null;
+    [SerializeField, Tooltip("TMP - 미리 받아 두기 위함")]
+    TMP_Text _TMP_this = null;
+
     [Header("--- 정해진 값 ---")]
-    [SerializeField, Tooltip("RRT - Original posY - 연출 시작 및 종료 뒤를 위함")]
-    float _posY_org = 0.35f;
-    [SerializeField, Tooltip("RRT - Scale - 연출 시작 및 종료 뒤를 위함")]
-    float _scale_org = 0.1f;
+    [SerializeField, Tooltip("RRT - Original posY - 연출 시작 값")]
+    float _posY_start = 0.35f;
+    [SerializeField, Tooltip("RRT - Original posY - 연출 + 값")]
+    float _posY_plus = 0.15f;
+    [SerializeField, Tooltip("RRT - Scale - 연출 시작 값")]
+    Vector3 _vec3_scale_start = new Vector3(0.01f, 0.01f, 0.01f);
+    [SerializeField, Tooltip("RRT - Scale - 연출 + 값")]
+    Vector3 _vec3_scale_plus = new Vector3(0.15f, 0.15f, 0.15f);
+    [SerializeField, Tooltip("RRT - Scale - 연출 종료 값")]
+    Vector3 _vec3_scale_end = new Vector3(0.1f, 0.1f, 0.1f);
+
+    Sequence _effect = null;
 
     public void Init(int damage)
     {
-        // Tween 뒤 -> Managers.PoolM.PushToPool(gameObject);
+        _TMP_this.text = damage.ToString();
+
+        if (_effect == null)
+        {
+            _rtr_this.anchoredPosition = new Vector3(0f, _posY_start, 0f);
+
+            _effect = DOTween.Sequence();
+
+            _effect.Append(transform.DOScale(_vec3_scale_plus, 0.2f));
+            _effect.Append(transform.DOScale(_vec3_scale_end, 0.2f));
+            _effect.Append(_rtr_this.DOAnchorPosY(_posY_start + _posY_plus, 0.3f));
+            _effect.Join(_TMP_this.DOFade(0f, 0.3f)).OnComplete(() => Managers.PoolM.PushToPool(gameObject));
+        }
     }
 
     private void OnDisable()
@@ -28,7 +55,15 @@ public class TMP_Damage : MonoBehaviour
 
     public void Clear()
     {
-        // 위치 초기화
+        if (_effect != null)
+        {
+            DOTween.Kill(transform);
+            _effect = null;
+        }
+
+        _rtr_this.anchoredPosition = new Vector3(0f, _posY_start, 0f);
+        _rtr_this.localScale = _vec3_scale_start;
+        _TMP_this.alpha = 1f;
     }
 
 #if UNITY_EDITOR
